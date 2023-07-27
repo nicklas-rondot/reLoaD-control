5c7c856aa86d53557d95c4064789a6b74133b1a6
nicklas-rn <nicklas.rondot@gmail.com>
2023-07-27 22:00:25 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  mode: 'jit',
+  content: ["./templates/**/*.{html,htm}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
