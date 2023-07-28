bb440b326f6c0f2b1f812658813b19d1729e4603
nicklas-rn <nicklas.rondot@gmail.com>
2023-07-28 23:10:46 +0200

    feat: add module page

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/modules')
+def modules():
+    return render_template('modules.html')
+
+@app.route('/create_module_overlay')
+def create_module_overlay():
+    return render_template('module_overlay_create.html')
+
 if __name__ == '__main__':
     app.run()
